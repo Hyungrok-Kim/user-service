7fbd7360241301baec22bf50781034fbbf92568d
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-06-20 16:49:14 +0900

    COMMIT

--- src/main/java/per/khr/main/userservice/dto/User.java
+++ src/main/java/per/khr/main/userservice/dto/User.java
@@ -1,4 +1,13 @@
 package per.khr.main.userservice.dto;
 
+import java.sql.Date;
+
 public class User {
+    private String email;
+    private String name;
+    private String password;
+    private String userId;
+    private Date createdAt;
+
+
 }
--- src/main/java/per/khr/main/userservice/vo/ResponseUser.java
+++ src/main/java/per/khr/main/userservice/vo/ResponseUser.java
@@ -1,4 +1,5 @@
 package per.khr.main.userservice.vo;
 
+// 주로 JSON으로 받을 변환 데이터
 public class ResponseUser {
 }
