29e959d58a067b07c6d39c4267f2cdf066b56538
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-06-20 16:04:51 +0900

    COMMIT

---
+++ src/test/java/per/khr/main/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package per.khr.main.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
