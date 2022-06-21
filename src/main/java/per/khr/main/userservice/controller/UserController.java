1d68c65ebd670f9a55eb55b2f1df238f01f3e772
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-06-21 10:55:42 +0900

    COMMIT

--- src/main/java/per/khr/main/userservice/controller/UserController.java
+++ src/main/java/per/khr/main/userservice/controller/UserController.java
@@ -1,7 +1,31 @@
 package per.khr.main.userservice.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import per.khr.main.userservice.vo.RequestUser;
+import per.khr.main.userservice.vo.ResponseUser;
+
+import java.util.List;
 
 @RestController
 public class UserController {
+    @GetMapping("/users")
+    public List<ResponseUser> getUsers() {
+
+        return null;
+    }
+
+    @PostMapping("/users")
+    public String createUser(@RequestBody RequestUser user) {
+        return "created success";
+    }
+
+    @PutMapping("/users/{userId}")      // PutMapping에 지정한 key 값
+    public String modifyUser(@PathVariable("userId") RequestUser user) {
+        return "modify success";
+    }
+
+    @DeleteMapping("/users/{userId}")
+    public String deleteUser(@PathVariable("userId") RequestUser user) {
+        return "delete success";
+    }
 }
