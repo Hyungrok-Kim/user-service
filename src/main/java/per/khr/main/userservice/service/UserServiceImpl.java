e020cc891d4d9920f7d65f95e41dd0c3091912b5
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-06-21 14:54:54 +0900

    COMMIT

--- src/main/java/per/khr/main/userservice/service/UserServiceImpl.java
+++ src/main/java/per/khr/main/userservice/service/UserServiceImpl.java
@@ -1,10 +1,25 @@
 package per.khr.main.userservice.service;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.internal.bytebuddy.matcher.StringMatcher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import per.khr.main.userservice.dao.UserDao;
+import per.khr.main.userservice.dao.UserEntity;
 import per.khr.main.userservice.dto.UserDto;
+
+import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private UserDao userDao;
+
+    @Autowired
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
 
     /**
      * userDto -> userEntity 객체로 변환 후 dao에 insert
@@ -14,6 +29,14 @@ public class UserServiceImpl implements UserService {
      */
     @Override
     public UserDto createUser(UserDto userDto) {
+        // UUID로 userId UNIQUE하게 세팅
+        userDto.setUserId(UUID.randomUUID().toString());
+
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserEntity userEntity = mapper.map(userDto, UserEntity.class);
+
         return null;
     }
 }
--- src/main/java/per/khr/main/userservice/vo/RequestUser.java
+++ src/main/java/per/khr/main/userservice/vo/RequestUser.java
@@ -6,7 +6,7 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-@Data
+//@Data
 public class RequestUser {
 
     @NotNull(message="Email cannot be null") // NotNull일 경우 메시지
@@ -19,4 +19,28 @@ public class RequestUser {
     @NotNull(message="Password cannot be null")
     @Size(min=8, message="Password must be equal or grater than 8 characters")
     private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
