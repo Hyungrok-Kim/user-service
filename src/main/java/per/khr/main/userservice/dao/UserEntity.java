85440aabc74d18d54b64c031a13d05e0f07a1e0d
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-06-21 15:40:39 +0900

    COMMIT

--- src/main/java/per/khr/main/userservice/dao/UserEntity.java
+++ src/main/java/per/khr/main/userservice/dao/UserEntity.java
@@ -1,4 +1,59 @@
 package per.khr.main.userservice.dao;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 50, unique = true)
+    private String email;
+    @Column(length = 50)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String userId;
+
+    private String encryptedPassword;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getEncryptedPassword() {
+        return encryptedPassword;
+    }
+
+    public void setEncryptedPassword(String encryptedPassword) {
+        this.encryptedPassword = encryptedPassword;
+    }
 }
