4fedb21daad58193870c889d51c8dba470b58f8c
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-06-21 11:04:31 +0900

    COMMIT

--- src/main/java/per/khr/main/userservice/controller/UserController.java
+++ src/main/java/per/khr/main/userservice/controller/UserController.java
@@ -7,24 +7,59 @@ import per.khr.main.userservice.vo.ResponseUser;
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
+    @GetMapping("/health_check")
+    public String status() {
+        return String.format("user-service connected");
+    }
+
+    /**
+     * 사용자 목록 조회
+     * @return
+     */
     @GetMapping("/users")
     public List<ResponseUser> getUsers() {
 
         return null;
     }
 
-    @PostMapping("/users")
+    /**
+     * 사용자 가입
+     * @param user : 사용자정보
+     * @return
+     */
+    @PostMapping("/")
     public String createUser(@RequestBody RequestUser user) {
         return "created success";
     }
 
-    @PutMapping("/users/{userId}")      // PutMapping에 지정한 key 값
+    /**
+     * 유저 정보 조회
+     * @param userId : 사용자 아이디
+     * @return
+     */
+    @GetMapping("/{userId}")
+    public ResponseUser getUser(@PathVariable("userId") String userId) {
+        return null;
+    }
+
+    /**
+     * 유저 정보 수정
+     * @param user : 사용자 정보
+     * @return
+     */
+    @PutMapping("/{userId}")      // PutMapping에 지정한 key 값
     public String modifyUser(@PathVariable("userId") RequestUser user) {
         return "modify success";
     }
 
-    @DeleteMapping("/users/{userId}")
+    /**
+     * 유저 정보 삭제
+     * @param user : 사용자 정보
+     * @return
+     */
+    @DeleteMapping("/{userId}")
     public String deleteUser(@PathVariable("userId") RequestUser user) {
         return "delete success";
     }
