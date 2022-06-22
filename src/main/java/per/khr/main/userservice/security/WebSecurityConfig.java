9229b1b28f236858c5fa5e10fae4cbc436521865
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-06-22 11:36:32 +0900

    COMMIT

---
+++ src/main/java/per/khr/main/userservice/security/WebSecurityConfig.java
@@ -0,0 +1,11 @@
+package per.khr.main.userservice.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+    }
+}
