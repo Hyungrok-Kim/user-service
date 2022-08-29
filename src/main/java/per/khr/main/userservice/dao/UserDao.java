008dd35d60abca627cb0a3421f607287595436d3
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2022-08-29 15:40:00 +0900

    [수정] UserDao: 주석 삭제

--- src/main/java/per/khr/main/userservice/dao/UserDao.java
+++ src/main/java/per/khr/main/userservice/dao/UserDao.java
@@ -10,6 +10,6 @@ import java.util.List;
  * -> UserDao를 JPA의 라이프사이클에 맞춰 관리하겠다.
  */
 public interface UserDao extends CrudRepository<UserEntity, Long> {
-    UserEntity findByEmail(String email); // CrudRepository를 상속받았으니
-    List<UserEntity> findAll();// JPA에서 where절에 email을 넣어서 쿼리 자동 생성해줍니다~
+    UserEntity findByEmail(String email); // CrudRepository를 상속받았으니 JPA에서 where절에 email을 넣어서 쿼리 자동 생성해줍니다~
+    List<UserEntity> findAll();
 }
